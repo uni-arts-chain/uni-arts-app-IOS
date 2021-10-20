b3374eb7296b6660cef3f936d44aad93c620270a
jielian-01 <chanbangye@gmail.com>
2021-10-20 15:06:00 +0800

    Adapt iOS 15

--- CloudArtChain/CloudArtChain/JLBase/JLNavigationViewController.m
+++ CloudArtChain/CloudArtChain/JLBase/JLNavigationViewController.m
@@ -18,17 +18,30 @@
     [super viewDidLoad];
     // 设置导航条不透明
     self.navigationBar.translucent = NO;
-    // 设置导航条背景色
-    self.navigationBar.barTintColor = JL_color_white_ffffff;
     // tintColor(这里主要调整返回箭头颜色)
     self.navigationBar.tintColor = JL_color_fontdeep;
     // 设置导航条title颜色及字体
-    NSDictionary *attrDict = @{NSFontAttributeName: kFontPingFangSCRegular(18)};
-    self.navigationBar.titleTextAttributes = attrDict;
-    self.navigationBar.titleTextAttributes = @{NSForegroundColorAttributeName: JL_color_gray_333333};
-    // 去掉navigationBar底部灰线
-    self.navigationBar.shadowImage = [UIImage new];
-    [self.navigationBar setBackgroundImage:[UIImage new] forBarMetrics:UIBarMetricsDefault];
+    NSDictionary *attrDict = @{NSFontAttributeName: kFontPingFangSCRegular(18), NSForegroundColorAttributeName: JL_color_gray_333333};
+    if (@available(iOS 13.0, *)) {
+        UINavigationBarAppearance *navigationBarAppearance = [[UINavigationBarAppearance alloc] init];
+        navigationBarAppearance.backgroundColor = JL_color_white_ffffff;
+        navigationBarAppearance.shadowColor = [UIColor clearColor];
+        navigationBarAppearance.shadowImage = [UIImage new];
+        navigationBarAppearance.backgroundImage = [UIImage new];
+        navigationBarAppearance.titleTextAttributes = attrDict;
+        if (@available(iOS 15.0, *)) {
+            self.navigationBar.scrollEdgeAppearance = navigationBarAppearance;
+        }
+        self.navigationBar.standardAppearance = navigationBarAppearance;
+    }else {
+        // 设置导航条背景色
+        self.navigationBar.barTintColor = JL_color_white_ffffff;
+        
+        self.navigationBar.titleTextAttributes = attrDict;
+        // 去掉navigationBar底部灰线
+        self.navigationBar.shadowImage = [UIImage new];
+        [self.navigationBar setBackgroundImage:[UIImage new] forBarMetrics:UIBarMetricsDefault];
+    }
 }
 
 - (void)pushViewController:(UIViewController *)viewController animated:(BOOL)animated {
